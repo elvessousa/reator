9a4d89442cfcda3efef88afe196d2250d2ea3490
Elves Sousa <elves.sousa@gmail.com>
2022-01-03 15:58:23 -0300

    Messages

--- src/messages/warnings.rs
+++ src/messages/warnings.rs
@@ -1,5 +1,6 @@
 // Warnings
 pub const WARN_INVALID_RN_STYLE: &str = "React Native doesn't accept CSS or Sass modules.";
+pub const WARN_INVALID_RN_ONLY_STYLE: &str = "RN Styles available only to React Native Components.";
 pub const WARN_CONTEXT_STYLING: &str = "One does not simply style a Context API file.";
 pub const WARN_NDOC_STYLING: &str = "Styling a Custom Next Document file is useless.";
 pub const WARN_INVALID_STYLE: &str =
@@ -7,6 +8,5 @@ pub const WARN_INVALID_STYLE: &str =
 
 // Infos
 pub const INFO_TS: &str = "You are using TypeScript.";
-pub const INFO_NEVER_HAPPEN: &str = "Ignoring it for it now. Let's pretend this never happened.";
 pub const INFO_PROJECT_ROOT: &str =
     "Make sure you are on a valid react project, and running this tool from its root directory.";
